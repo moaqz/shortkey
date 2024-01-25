0a8b0f968f46e7d354c5c2708ef2d9799e15eaf1
moaqz <goz.matu@gmail.com>
2024-01-25 18:22:47 -0300

    add og image to metadata and add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,33 @@ import type { Metadata } from "next";
 import { BASE_URL } from "~/lib/constants";
 import "~/styles/globals.css";
 
-export const metadata: Metadata = {
+const METADATA = {
   title: "Shortkey",
   description: "Minimalistic and open-source URL shortener.",
+  images: [
+    {
+      url: `${BASE_URL}/og-image.png`,
+      width: 1200,
+      height: 630,
+    },
+  ],
+};
+
+export const metadata: Metadata = {
+  title: METADATA.title,
+  description: METADATA.description,
   openGraph: {
-    title: "Shortkey",
-    description: "Minimalistic and open-source URL shortener.",
+    title: METADATA.title,
+    description: METADATA.description,
     type: "website",
     url: BASE_URL,
+    images: [...METADATA.images],
+  },
+  twitter: {
+    title: METADATA.title,
+    description: METADATA.description,
+    card: "summary_large_image",
+    images: [...METADATA.images],
   },
 };
 
