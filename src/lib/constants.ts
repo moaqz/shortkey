4013cd72c23ca18129bb37091c3ad68a02a12919
moaqz <goz.matu@gmail.com>
2024-01-30 19:16:20 -0300

    remove unused constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,16 +1,9 @@
 export const SOCIAL_LINKS = {
-  repository: "https://github.com/moaqz",
+  repository: "https://github.com/moaqz/shortkey",
   portfolio: "https://moaqz.tech",
 } as const;
 
 export const LINKS_TABLE = "links";
-
-export const STATES = {
-  ERROR: "error",
-  SUCCESS: "success",
-  LOADING: "loading",
-  IDLE: "idle",
-};
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
